ea09c73ac7fd539c8f283a567cbe04631bf69b10
dengagaga <134398955+dengagaga@users.noreply.github.com>
2023-08-27 18:49:07 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+  
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+  });
